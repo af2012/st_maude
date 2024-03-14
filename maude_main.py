e6fdf7498e9151efc07d2e4800444aabe549beca
af2012 <af2012@gmail.com>
2024-03-14 09:07:06 -0400

    Update maude_main.py

--- maude_main.py
+++ maude_main.py
@@ -5,3 +5,9 @@ import streamlit as st
 st.set_page_config(layout='wide')
 
 st.title('FDA MAUDE Search')
+
+'''
+This test app lets you search the FDA MAUDE free-text.
+
+If you have any questions, please reach out to Allan.Fong@medstar.net
+'''
